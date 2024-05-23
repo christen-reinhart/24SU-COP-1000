ca4fc6d3069d4e470fafa4fef893876d8aef82d3
christen-reinhart <christen.reinhart@yahoo.com>
2024-05-23 10:09:52 -0400

    Billing Hierarchy

--- Assignment-2-1/Billing_Hierarchy.py
+++ Assignment-2-1/Billing_Hierarchy.py
@@ -1,9 +1,20 @@
 #!/user/bin/env python3
 
-# Script name Assignment 1-1
+# Script name Assignment 2-1
 # Author Name Christen Reinhart
-# Date of Latest Revision 05/15/2024
-# Sources https://replit.com/@christenreinhar/COP1000Assignment-1-1#main.py 
-# Purpose In Python, Print message  
+# Date of Latest Revision 05/23/2024
+# Sources 
+# Purpose In Python, Billing  
 
 #Begin
+
+# Define usage and costs
+minutes = 200  # Number of minutes used
+minutecost = 1  # Cost per minute
+textmessage = 300  # Number of text messages sent
+textcost = 2  # Cost per text message
+
+# Calculate the total bill
+totalbill = calculate_total_bill(minutes, minutecost, textmessage, textcost)
+
+#End
