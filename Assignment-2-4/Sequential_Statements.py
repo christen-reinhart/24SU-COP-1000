4a0e21469fbbc89297fc6d25a0aae3acbb8e12a6
christen-reinhart <christen.reinhart@yahoo.com>
2024-05-23 15:28:15 -0400

    ASSIGN-2-4

--- Assignment-2-4/Sequential_Statements.py
+++ Assignment-2-4/Sequential_Statements.py
@@ -1,11 +1,24 @@
+#!/usr/bin/env python3
+
+# Script name: Assignment 2-4
+# Author Name: Christen Reinhart
+# Date of Latest Revision: 05/23/2024
+# Purpose: Arithmetic with Python
+
+# This Calculates paycheck
+# Input:  paycheck amounts and percentages
+# Output: Calculate pay amounts
+
+#Begin
+
 # Initialize variables with input from the user
 salary = float(input("Enter the weekly salary: "))
 numDependents = int(input("Enter the number of dependents: "))
 
-# Calculate state withholding tax at 6.5%
+# Calculate state tax witholding at 6.5%
 stateTax = salary * 0.065
 
-# Calculate federal withholding tax at 28.0%
+# Calculate federal tax witholding at 28.0%
 federalTax = salary * 0.28
 
 # Calculate dependent deductions at 2.5% of the salary for each dependent
@@ -24,3 +37,4 @@ print("Dependent Deductions: $" + str(dependentDeduction))
 print("Salary: $" + str(salary))
 print("Take Home Pay: $" + str(takeHomePay))
 
+#End
