02bf4a890bb8c1cb3aa966832d193fa96027abe1
christen-reinhart <christen.reinhart@yahoo.com>
2024-05-23 12:09:27 -0400

    Billing Hierarchy

--- Assignment-2-1/Billing_Hierarchy.py
+++ Assignment-2-1/Billing_Hierarchy.py
@@ -1,21 +1,29 @@
-#!/user/bin/env python3
+#!/usr/bin/env python3
 
-# Script name Assignment 2-1
-# Author Name Christen Reinhart
-# Date of Latest Revision 05/23/2024
-# Sources 
-# Purpose In Python, Calculate Phone Billing  
+# Script name: Assignment 2-1
+# Author Name: Christen Reinhart
+# Date of Latest Revision: 05/23/2024
+# Purpose: Calculate Phone Billing in Python
 
-#Begin
+#start
 
-# Define usage and costs
+# Declarations: define usage and costs
 minutes = 200  # Number of minutes used
 minutecost = 1  # Cost per minute
 textmessage = 300  # Number of text messages sent
 textcost = 2  # Cost per text message
-num SALES_TAX_AMOUNT = 0.06
-taxAmount = totalbill * SALES_TAX_AMOUNT
-# Calculate the total bill
-totalbill = calculate_total_bill(minutes, minutecost, textmessage, textcost)
-print("totalbill")
+SALES_TAX_AMOUNT = 0.06  # Sales tax rate
+
+# Calculate the subtotal
+subtotal = (minutes * minutecost) + (textmessage * textcost)
+
+# Calculate the tax amount
+tax_amount = subtotal * SALES_TAX_AMOUNT
+
+# Calculate the final total bill
+total_bill = subtotal + tax_amount
+
+# Output the total bill
+print("Total phone bill:", total_bill)
+
 #End
