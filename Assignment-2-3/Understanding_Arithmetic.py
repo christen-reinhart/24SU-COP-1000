396fd5bb3d5282eb2dbfe8271971532de0b224c7
christen-reinhart <christen.reinhart@yahoo.com>
2024-05-23 14:04:04 -0400

    Math

--- Assignment-2-3/Understanding_Arithmetic.py
+++ Assignment-2-3/Understanding_Arithmetic.py
@@ -4,11 +4,20 @@ itemName = "TV Stand"
 retailPrice = 325.00
 wholesalePrice = 200.00
 
-# Write your assignment statements here for profit, salePrice, and saleProfit
+# Calculate profit
+profit = retailPrice - wholesalePrice
 
+# Calculate sale price (assuming a 10% discount)
+salePrice = retailPrice * 0.90  # 10% discount is equivalent to 90% of the retail price
+
+# Calculate sale profit
+saleProfit = salePrice - wholesalePrice
+
+# Print the results
 print("Item Name: " + itemName)
 print("Retail Price: $" + str(retailPrice))
 print("Wholesale Price: $" + str(wholesalePrice))
 print("Profit: $" + str(profit))
 print("Sale Price: $" + str(salePrice))
 print("Sale Profit: $" + str(saleProfit))
+
