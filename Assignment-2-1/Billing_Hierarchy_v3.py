3a7e4dc53c5eb28c89d455d667f1088af85b799c
christen-reinhart <christen.reinhart@yahoo.com>
2024-05-24 16:13:10 -0400

    Pseudo_Code

---
+++ Assignment-2-1/Billing_Hierarchy_v3.py
@@ -0,0 +1,28 @@
+#Understanding Arithmetic
+
+start 
+
+   Declarations
+	string prompt 
+	num balance
+   output prompt "enter pay amount"
+   input "pay amount"
+   output prompt "enter number of people"
+   Input "number of people"
+   output "pay values"
+   	
+End 
+
+#Assigning Age Variables
+
+Start 
+
+   Declarations 
+   string  "current age" 
+   string number "current year"
+   output prompt "enter currnet age"
+   input "age"
+   output prompt "enter currnt year"
+   input "year" 
+   
+End
