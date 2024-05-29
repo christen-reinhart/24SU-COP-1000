f28b34daf89abd58e69dae944af700bf1de0493f
christen-reinhart <christen.reinhart@yahoo.com>
2024-05-29 16:06:24 -0400

    Flowchartv2

--- Assignment-3-1/Structured_Flowchart.py
+++ Assignment-3-1/Structured_Flowchart.py
@@ -1,50 +1,30 @@
 BEGIN
-    // Starting point: Home
+    // Start from home
     
-    // Decision 1: Check if you are at the starting point
-    IF current_location IS NOT "Home" THEN
-        PRINT "Error: You need to start from Home."
-        EXIT
-    ENDIF
-    
-    // Loop 1: Move north on Rome Ave until you reach Columbus
-    WHILE current_location IS NOT "Rome Ave and Columbus"
-        MOVE_NORTH_ON_Rome_Ave()
+    // Go north on Rome Ave until you reach Columbus
+    WHILE not at Columbus
+        GO north on Rome Ave
     ENDWHILE
     
-    // Decision 2: Check if you reached Columbus
-    IF current_location IS "Rome Ave and Columbus" THEN
-        // Turn left to go west on Columbus
-        TURN_LEFT()
-        MOVE_WEST_ON_Columbus()
-    ELSE
-        PRINT "Error: You missed Columbus."
-        EXIT
-    ENDIF
-    
-    // Loop 2: Move west on Columbus until you reach Dale Mabry
-    WHILE current_location IS NOT "Columbus and Dale Mabry"
-        MOVE_WEST_ON_Columbus()
+    // Turn left onto Columbus and go west
+    TURN left onto Columbus
+    WHILE not at Dale Mabry
+        GO west on Columbus
     ENDWHILE
     
-    // Turn right to go north on Dale Mabry
-    TURN_RIGHT()
-    MOVE_NORTH_ON_Dale_Mabry()
-    
-    // Loop 3: Move north on Dale Mabry until you reach Tampa Bay Blvd
-    WHILE current_location IS NOT "Dale Mabry and Tampa Bay Blvd"
-        MOVE_NORTH_ON_Dale_Mabry()
+    // Turn right onto Dale Mabry and go north
+    TURN right onto Dale Mabry
+    WHILE not at Tampa Bay Blvd
+        GO north on Dale Mabry
     ENDWHILE
     
-    // Turn left to go west on Tampa Bay Blvd
-    TURN_LEFT()
-    MOVE_WEST_ON_Tampa_Bay_Blvd()
-    
-    // Loop 4: Move west on Tampa Bay Blvd until you see the school on your right
-    WHILE school_location IS NOT "On your right"
-        MOVE_WEST_ON_Tampa_Bay_Blvd()
+    // Turn left onto Tampa Bay Blvd and find the school
+    TURN left onto Tampa Bay Blvd
+    WHILE school is not on the right
+        GO west on Tampa Bay Blvd
     ENDWHILE
     
     // Arrive at the school
     PRINT "You have arrived at the school."
 END
+
