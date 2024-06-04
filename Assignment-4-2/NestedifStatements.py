3482faf07efbfb89507d87593c44a5bc48f52e24
christen-reinhart <christen.reinhart@yahoo.com>
2024-06-04 17:41:31 -0400

    NestedifStatements

---
+++ Assignment-4-2/NestedifStatements.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+# Script name: Assignment 4-2
+# Author Name: Christen Reinhart
+# Date of Latest Revision: 06/04/2024
+# Purpose: Calculculate Bonus
+
+# This Calculates Bonus
+# Input: Productivity Bonus Numbers
+# Output: Calculate Bonus
+
+# start
+
+# Input data
+employee_name = input("Enter the employee's name: ")
+num_shifts = int(input("Enter the number of shifts: "))
+num_transactions = int(input("Enter the number of transactions: "))
+transaction_value = float(input("Enter the transaction dollar value: "))
+
+# Calculate productivity score
+productivity_score = (transaction_value / num_transactions) / num_shifts
+
+# Determine the bonus using nested if statements
+if productivity_score <= 30:
+    bonus = 50.00
+else:
+    if 31 <= productivity_score <= 69:
+        bonus = 75.00
+    else:
+        if 70 <= productivity_score <= 199:
+            bonus = 100.00
+        else:
+            if productivity_score >= 200:
+                bonus = 200.00
+                
+# end
