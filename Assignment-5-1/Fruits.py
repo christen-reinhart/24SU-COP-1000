e7ad21781a4d3c51cae97f424c9c9e5fe5ec0df1
christen-reinhart <christen.reinhart@yahoo.com>
2024-06-11 10:32:04 -0400

    Fruit

---
+++ Assignment-5-1/Fruits.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Script name: Assignment 5 Fruit
+# Author Name: Christen Reinhart
+# Date of Latest Revision: 06/11/2024
+# Purpose: Fruit Loop
+
+# Performs Loop
+# Input: Products
+# Output: Print Fruit
+
+# start
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+# end
