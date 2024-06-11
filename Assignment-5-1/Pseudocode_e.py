9539c2fdcde53f70d410d860a92bf65900f92678
christen-reinhart <christen.reinhart@yahoo.com>
2024-06-11 12:08:27 -0400

    Pseudocode_e

--- Assignment-5-1/Pseudocode_e.py
+++ Assignment-5-1/Pseudocode_e.py
@@ -1,24 +1,28 @@
 #!/usr/bin/env python3
 
-# Script name: Assignment Pseudocode e
+# Script name: Assignment Pseudocode d
 # Author Name: Christen Reinhart
 # Date of Latest Revision: 06/11/2024
 # Purpose: Loop
 
 # Performs Loop
-# Input: 2, 5, 6, 9
-# Output: j, k, m, n
+# Input: 2, 5, 9
+# Output: j, k, n
 
 # start
 
 # Constants
+j = 2
+k = 5
+n = 9
 
-g = 4
-h = 6
+m = 6
+while j <= k:
+    while m < n:
+        print("Goodbye")
+        m = m + 1
+    j = j + 1
 
-while g <= h:
-    g = g + 1
-    
-print(g, h)
+print(j, k, n)
 
 # end
