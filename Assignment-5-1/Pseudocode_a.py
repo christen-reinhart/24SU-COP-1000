a8dda9d8d575aa9f237d17ff7e331e900ba293d2
christen-reinhart <christen.reinhart@yahoo.com>
2024-06-11 10:59:01 -0400

    Pseudocode_a

---
+++ Assignment-5-1/Pseudocode_a.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Script name: Assignment Pseudocode a
+# Author Name: Christen Reinhart
+# Date of Latest Revision: 06/11/2024
+# Purpose: Loop
+
+# Performs Loop
+# Input: 1, 2, 5
+# Output: a, b, c
+
+# start
