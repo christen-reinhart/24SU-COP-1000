72d04b7be4820f2f962f3135c1b30171eb3895ac
christen-reinhart <christen.reinhart@yahoo.com>
2024-06-13 10:47:12 -0400

    Pseudocode_d

--- Assignment-5-1/Pseudocode_d.py
+++ Assignment-5-1/Pseudocode_d.py
@@ -16,14 +16,20 @@ j = 2
 k = 5
 n = 9
 
+# Variable initialization
 m = 6
+
+# Outer loop until j is greater than k
 while j <= k:
+    # Reset m to 6 at the beginning of each outer loop iteration
     m = 6
+    # Inner loop until m is no longer less than n
     while m < n:
-        print("Goodbye")
-        m = m + 1
+        print("Goodbye")  # Print "Goodbye"
+        m = m + 1  # Increment m by 1
+    # Increment j by 1 at the end of each outer loop iteration
     j = j + 1
 
-
 # end
+
 
