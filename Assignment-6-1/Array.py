51cbbb8a2c21e8741feb0432d0684a6681f337e8
christen-reinhart <christen.reinhart@yahoo.com>
2024-06-19 14:46:37 -0400

    Array_Function

--- Assignment-6-1/Array.py
+++ Assignment-6-1/Array.py
@@ -10,31 +10,9 @@
 # Output: Pending
 
 # start
-    # declarations
-    # string name
-    # string QUIT = "ZZZ"
-# output "Enter name"
-# input name
-# while name <> QUIT
-    # output "Hello ", name
-    # input name
-# endwhile
-# output "Goodbye"
-# stop
+# names
+names = ["Christen", "Joe", "James", "Brian"]
+print(names)    
 
-# Initialize variables
-QUIT = "ZZZ"
 
-# Prompt user to enter a name
-name = input("Enter name: ")
 
-# Loop until the entered name is equal to QUIT
-while name != QUIT:
-    # Output greeting with the entered name
-    print("Hello", name)
-    
-    # Prompt user to enter a name again
-    name = input("Enter name: ")
-
-# Output goodbye message
-print("Goodbye")
