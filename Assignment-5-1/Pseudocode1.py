d537d26cdd07fe6abc415a1e125986e1bc14d5d0
christen-reinhart <christen.reinhart@yahoo.com>
2024-06-19 13:04:18 -0400

    Looping5-9_COP-1000

--- Assignment-5-1/Pseudocode1.py
+++ Assignment-5-1/Pseudocode1.py
@@ -11,15 +11,33 @@
 
 # start
 
-start
-    declarations
-    string name
-    string QUIT = "ZZZ"
-output "Enter name"
-input name
-while name <> QUIT
-    output "Hello ", name
-    input name
-endwhile
-output "Goodbye"
-stop
+# start
+    # declarations
+    # string name
+    # string QUIT = "ZZZ"
+# output "Enter name"
+# input name
+# while name <> QUIT
+    # output "Hello ", name
+    # input name
+# endwhile
+# output "Goodbye"
+# stop
+
+# Initialize variables
+QUIT = "ZZZ"
+
+# Prompt user to enter a name
+name = input("Enter name: ")
+
+# Loop until the entered name is equal to QUIT
+while name != QUIT:
+    # Output greeting with the entered name
+    print("Hello", name)
+    
+    # Prompt user to enter a name again
+    name = input("Enter name: ")
+
+# Output goodbye message
+print("Goodbye")
+
