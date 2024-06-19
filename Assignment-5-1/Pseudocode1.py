4db854308480c5f2b33eb3ba29a5472087dae268
christen-reinhart <christen.reinhart@yahoo.com>
2024-06-19 12:58:31 -0400

    Looping5-9_COP-1000

---
+++ Assignment-5-1/Pseudocode1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Script name: Assignment 5
+# Author Name: Christen Reinhart
+# Date of Latest Revision: 06/11/2024
+# Purpose: Pseudocode for Loop
+
+# This Calculates Bonus
+# Input: Productivity Bonus Numbers
+# Output: Calculate Bonus
+
+# start
+
+start
+    declarations
+    string name
+    string QUIT = "ZZZ"
+output "Enter name"
+input name
+while name <> QUIT
+    output "Hello ", name
+    input name
+endwhile
+output "Goodbye"
+stop
