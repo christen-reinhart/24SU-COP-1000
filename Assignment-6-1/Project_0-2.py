8d246393bc682001d7491e0809e7f0d42e70b79a
christen-reinhart <christen.reinhart@yahoo.com>
2024-06-20 11:17:22 -0400

    Array_Trucks

--- Assignment-6-1/Project_0-2.py
+++ Assignment-6-1/Project_0-2.py
@@ -4,16 +4,12 @@
 # Author Name: Christen Reinhart
 # Date of Latest Revision: 06/21/2024
 # Purpose: Arrays
-# Reference Chat GPT: https://chatgpt.com/share/6e22c821-64b7-4926-83c7-f5743cb5a143
-# Initial Draft With Assistance
 
-# Input: 
-# Output: Array
+# Input: Array with Trucks for car Finder
+# Output: Options
 
 # start
 
-
-# carfinder.py
 
 # List of allowed vehicles (consider storing in a file for easier updates)
 AllowedVehiclesList = ['Ford F-150', 'Chevrolet Silverado', 'Tesla CyberTruck', 'Toyota Tundra', 'Nissan Titan']
