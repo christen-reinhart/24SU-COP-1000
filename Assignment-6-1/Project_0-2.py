8ff35c009881f0bad902b86bb4b3bbfe3f7639b7
christen-reinhart <christen.reinhart@yahoo.com>
2024-06-21 10:08:37 -0400

    AutoCountry_v2

--- Assignment-6-1/Project_0-2.py
+++ Assignment-6-1/Project_0-2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-# Script name: Project 0-1 
+# Script name: Project 0-2 
 # Author Name: Christen Reinhart
-# Date of Latest Revision: 06/20/2024
+# Date of Latest Revision: 06/24/2024
 # Purpose: Vehicle Finder
 
 # Input: Array with Trucks for car Finder
@@ -10,32 +10,42 @@
 
 # start
 
+# CarFinder v0.2
 
-
-# List of authorized vehicles (consider storing in a file for easier updates)
-authorized_vehicles = ['Ford F-150', 'Chevrolet Silverado', 'Tesla CyberTruck', 'Toyota Tundra', 'Nissan Titan']
+# List of allowed vehicles
+allowed_vehicles = ['Ford F-150', 'Chevrolet Silverado', 'Tesla CyberTruck', 'Toyota Tundra', 'Nissan Titan']
 
 while True:
-  print("""
+    print("""
 ********************************
-AutoCountry Vehicle Finder v0.1
+AutoCountry Vehicle Finder v0.2
 ********************************
-Please Enter a choice:
+Please Enter the following number below from the following menu:
+
 1. PRINT all Authorized Vehicles
-2. Exit
+2. SEARCH for Authorized Vehicle
+3. Exit
 """)
+    
+    choice = input("Enter your choice: ").strip()
+    
+    if choice == '1':
+        print("\nThe AutoCountry sales manager has authorized the purchase and selling of the following vehicles:")
+        for vehicle in allowed_vehicles:
+            print(vehicle)
+        print()  # Add a newline for better readability
+    elif choice == '2':
+        search_vehicle = input("Please enter the full vehicle name: ").strip()
+        if search_vehicle in allowed_vehicles:
+            print(f"\n{search_vehicle} is an authorized vehicle")
+        else:
+            print(f"\n{search_vehicle} is not an authorized vehicle, if you received this in error please check the spelling and try again")
+        print()  # Add a newline for better readability
+    elif choice == '3':
+        print("Thank you for using the AutoCountry Vehicle Finder, good-bye!")
+        break
+    else:
+        print("Invalid choice. Please enter 1, 2, or 3.\n")
 
-  choice = input("Enter your choice (1 or 2): ").strip().upper()  # Handle both uppercase and lowercase input
 
-  if choice == '1':
-    print("\nThe AutoCountry sales manager has authorized the purchase and selling of the following vehicles:")
-    for vehicle in authorized_vehicles:
-      print(vehicle)
-    print()  # Add a newline for better readability
 
-  elif choice == '2':
-    print("Thank you for using the AutoCountry Vehicle Finder, good-bye!")
-    break
-
-  else:
-    print("Invalid choice. Please enter 1 or 2.\n")
