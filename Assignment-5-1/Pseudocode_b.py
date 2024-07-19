f5d6a12042fbb3bdf700c36108802a2da76e95c3
christen-reinhart <christen.reinhart@yahoo.com>
2024-07-19 19:09:28 -0400

    Pseudocode

--- Assignment-5-1/Pseudocode_b.py
+++ Assignment-5-1/Pseudocode_b.py
@@ -12,17 +12,17 @@
 # start
 
 # Constants
-d = 4
-e = 6
-f = 7
+# d = 4
+# e = 6
+# f = 7
 
 # Loop until d is greater than f
-while d <= f:
-    d = d + 1  # Increment d by 1
-    e = e - 1  # Decrement e by 1
+# while d <= f:
+# d = d + 1  # Increment d by 1
+# e = e - 1  # Decrement e by 1
 
 # Print the final values of d, e, and f
-print(d, e, f)
+# print(d, e, f)
 
 # end
 
@@ -34,8 +34,8 @@ print(d, e, f)
 # e = 6
 # f = 7
 # while d < f
-    # d = d + 1
-    # e = e - 1
+# d = d + 1
+# e = e - 1
 # endwhile
 # output d, e, f
 
@@ -48,11 +48,11 @@ print(d, e, f)
 
 # Loop until d is less than f
 # while d < f:
-    # Increment d by 1
-    # d = d + 1
+# Increment d by 1
+# d = d + 1
     
-    # Decrement e by 1
-    # e = e - 1
+# Decrement e by 1
+# e = e - 1
 
 # Output the values of d, e, and f
 # print("d:", d)
