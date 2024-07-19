ce099ad8cfb14eebdf8c7de202a8476c08e0919c
christen-reinhart <christen.reinhart@yahoo.com>
2024-07-19 15:40:46 -0400

    Project_0-6

--- Assignment-6-1/Project_0-6.py
+++ Assignment-6-1/Project_0-6.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-# Script name: Project 0-5 
+# Script name: Project 0-6 
 # Author Name: Christen Reinhart
-# Date of Latest Revision: 07/11/2024
+# Date of Latest Revision: 07/21/2024
 # Purpose: Vehicle Finder
 
 import os
