454793b9ab4316a51b6342975c3a719260d40ffd
christen-reinhart <christen.reinhart@yahoo.com>
2024-07-19 20:49:14 -0400

    Pseudocode

--- Assignment-3-1/Structured_Flowchart_v2.py
+++ Assignment-3-1/Structured_Flowchart_v2.py
@@ -1,50 +1,51 @@
-BEGIN
-    // Starting point: Home
-    
-    // Check if you are at home
-    IF you are not at home THEN
-        PRINT "You need to start from home."
-        STOP
-    ENDIF
-    
-    // Head north on Rome Ave until you reach Columbus
-    WHILE you are not at the intersection of Rome Ave and Columbus
-        GO north on Rome Ave
-    ENDWHILE
-    
-    // Check if you have reached Columbus
-    IF you have reached the intersection of Rome Ave and Columbus THEN
-        // Turn left to go west on Columbus
-        TURN left onto Columbus
-        CONTINUE west on Columbus
-    ELSE
-        PRINT "You missed Columbus."
-        STOP
-    ENDIF
-    
-    // Go west on Columbus until you reach Dale Mabry
-    WHILE you are not at the intersection of Columbus and Dale Mabry
-        CONTINUE west on Columbus
-    ENDWHILE
-    
-    // Turn right to go north on Dale Mabry
-    TURN right onto Dale Mabry
-    CONTINUE north on Dale Mabry
-    
-    // Head north on Dale Mabry until you reach Tampa Bay Blvd
-    WHILE you are not at the intersection of Dale Mabry and Tampa Bay Blvd
-        CONTINUE north on Dale Mabry
-    ENDWHILE
-    
-    // Turn left to go west on Tampa Bay Blvd
-    TURN left onto Tampa Bay Blvd
-    CONTINUE west on Tampa Bay Blvd
-    
-    // Look for the school on your right
-    WHILE the school is not on your right
-        CONTINUE west on Tampa Bay Blvd
-    ENDWHILE
-    
-    // Arrive at the school
-    PRINT "You have arrived at the school."
-END
+# begin
+# Starting point: Home
+
+# Check if you are at home
+# if you are not at home then
+# print "You need to start from home."
+# stop
+# end if
+
+# Head north on Rome Ave until you reach Columbus
+# while you are not at the intersection of Rome Ave and Columbus
+# go north on Rome Ave
+# end while
+
+# Check if you have reached Columbus
+# if you have reached the intersection of Rome Ave and Columbus then
+# Turn left to go west on Columbus
+# turn left onto Columbus
+# continue west on Columbus
+# else
+# print "You missed Columbus."
+# stop
+# end if
+
+# Go west on Columbus until you reach Dale Mabry
+# while you are not at the intersection of Columbus and Dale Mabry
+# continue west on Columbus
+# end while
+
+# Turn right to go north on Dale Mabry
+# turn right onto Dale Mabry
+# continue north on Dale Mabry
+
+# Head north on Dale Mabry until you reach Tampa Bay Blvd
+# while you are not at the intersection of Dale Mabry and Tampa Bay Blvd
+# continue north on Dale Mabry
+# end while
+
+# Turn left to go west on Tampa Bay Blvd
+# turn left onto Tampa Bay Blvd
+# continue west on Tampa Bay Blvd
+
+# Look for the school on your right
+# while the school is not on your right
+# continue west on Tampa Bay Blvd
+# end while
+
+# Arrive at the school
+# print "You have arrived at the school."
+# end
+
