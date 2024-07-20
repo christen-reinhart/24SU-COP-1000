97558701a24c675b229722330e70ec6f90bc2e1f
christen-reinhart <christen.reinhart@yahoo.com>
2024-07-20 09:06:14 -0400

    Psedudocode

--- Assignment-4-1/If_python.py
+++ Assignment-4-1/If_python.py
@@ -12,18 +12,18 @@ numChars = int(input("Enter the number of characters: "))
 woodType = input("Enter the type of wood (oak or pine): ").strip().lower()
 color = input("Enter the color of characters (black, white, or gold): ").strip().lower()
 
-# Initialize base charge
+# base charge
 charge = 35.00
 
-# Charge for additional characters if more than 5
+# additional characters if more than 5
 if numChars > 5:
     charge += (numChars - 5) * 4
 
-# Charge for type of wood
+# type of wood
 if woodType == "oak":
     charge += 20.00
 
-# Charge for color of characters
+# color of characters
 if color == "gold":
     charge += 15.00
 
