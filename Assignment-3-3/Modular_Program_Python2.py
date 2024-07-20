8c1baf03006d96a8046a4279f0c4ee052ca2576d
christen-reinhart <christen.reinhart@yahoo.com>
2024-07-20 09:24:22 -0400

    Pseudocode

--- Assignment-3-3/Modular_Program_Python2.py
+++ Assignment-3-3/Modular_Program_Python2.py
@@ -16,8 +16,6 @@
 
 #start
 
-# Constants
-
 validDate = True
 MIN_YEAR = 0
 MIN_MONTH = 1
@@ -25,16 +23,16 @@ MAX_MONTH = 12
 MIN_DAY = 1
 MAX_DAY = 31
 
-# Get input from user
+# input from user
 year = int(input("Enter year: "))
 month = int(input("Enter month: "))
 day = int(input("Enter day: "))
 
-# Check if date is valid
+# check date is valid
 if year <= MIN_YEAR or month < MIN_MONTH or month > MAX_MONTH or day < MIN_DAY or day > MAX_DAY:
     validDate = False
 
-# Output result
+# output 
 if validDate:
     print(f"{month}/{day}/{year} is a valid date.")
 else:
