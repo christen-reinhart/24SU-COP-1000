cdf797e3d1831ddc21d8893b8260824d5de546b5
christen-reinhart <christen.reinhart@yahoo.com>
2024-07-20 23:03:06 -0400

    Pseudocode

--- Assignment-2-4/Sequential_Statements.py
+++ Assignment-2-4/Sequential_Statements.py
@@ -3,34 +3,34 @@
 # Script name: Assignment 2-4
 # Author Name: Christen Reinhart
 # Date of Latest Revision: 05/23/2024
-# Purpose: Arithmetic with Python
+# Purpose: Calculates paycheck with Python
 
-# This Calculates paycheck
+# This 
 # Input:  paycheck amounts and percentages
 # Output: Calculate pay amounts
 
-#Begin
+# start
 
-# Initialize variables with input from the user
+# variables with input from the user
 salary = float(input("Enter the weekly salary: "))
 numDependents = int(input("Enter the number of dependents: "))
 
-# Calculate state tax witholding at 6.5%
+# state tax witholding at 6.5%
 stateTax = salary * 0.065
 
-# Calculate federal tax witholding at 28.0%
+# federal tax witholding at 28.0%
 federalTax = salary * 0.28
 
-# Calculate dependent deductions at 2.5% of the salary for each dependent
+# dependent deductions at 2.5% of the salary for each dependent
 dependentDeduction = salary * 0.025 * numDependents
 
-# Calculate total withholding as stateTax + federalTax + dependentDeduction
+# total withholding as stateTax + federalTax + dependentDeduction
 totalWithholding = stateTax + federalTax + dependentDeduction
 
-# Calculate take-home pay as salary - totalWithholding
+# take-home pay as salary - totalWithholding
 takeHomePay = salary - totalWithholding
 
-# Output statements
+# output statements
 print("State Tax: $" + str(stateTax))
 print("Federal Tax: $" + str(federalTax))
 print("Dependent Deductions: $" + str(dependentDeduction))
