981f5ccac61346c1411268293ecd7a57c0802984
christen-reinhart <christen.reinhart@yahoo.com>
2024-07-21 00:04:55 -0400

    Assignment-2-2

---
+++ Assignment-3-2/Assignment-3-2.py
@@ -0,0 +1,35 @@
+# !/usr/bin/env python3
+
+# Script name: Assignment 3-2
+# Author Name: Christen Reinhart
+# Date of Latest Revision: 07/20/2024
+# Purpose: Calculate accept or reject
+
+# start 
+
+# input data for test score and rank
+testScore = int(input("Enter the test score: "))
+classRank = int(input("Enter the class rank: "))
+
+# admission score print Accept or Reject
+if testScore >= 90:
+    if classRank >= 25:
+        print("Accept")
+    else:
+        print("Reject")
+else:
+    if testScore >= 80:
+        if classRank >= 50:
+            print("Accept")
+        else:
+            print("Reject")
+    else:
+        if testScore >= 70:
+            if classRank >= 75:
+                print("Accept")
+            else:
+                print("Reject")
+        else:
+            print("Reject")
+            
+# end
