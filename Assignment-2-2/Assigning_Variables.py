591e81ba135e803825377ca22dc321b0b0d85c6f
christen-reinhart <christen.reinhart@yahoo.com>
2024-07-20 23:12:46 -0400

    Assignment-2-2

--- Assignment-2-2/Assigning_Variables.py
+++ Assignment-2-2/Assigning_Variables.py
@@ -2,30 +2,28 @@
 
 # Script name: Assignment 2-2
 # Author Name: Christen Reinhart
-# Date of Latest Revision: 05/23/2024
+# Date of Latest Revision: 07/20/2024
 # Purpose: Assignment 2-2 Variables in Python 
 
 # This program calculates your age in the year 2050.
 # Input:  Age and current year
 # Output: Your current age followed by your age in 2050
 
-#Begin
-
-# Create your variables here
+# start
 
 # variable for currnet age
 myCurrentAge = 45
 
-#variable for current year
+# variable for current year
 currentYear = 2024
 
-#variable subtracts the currnet year from 2050
+# variable subtracts the currnet year from 2050
 myNewAge = myCurrentAge + (2050 - currentYear)
 
-#print for current age
+# print for current age
 print("My Current Age is " + str(myCurrentAge))
 
-#print for age in 2050
+# print for age in 2050
 print("I will be " + str(myNewAge) + " in 2050.")
 
-#End
+# end
